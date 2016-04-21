962b72b9f25ac5fb8700845e1c4662e35392086b
Alessio Bazzica <alessio.bazzica@gmail.com>
2016-04-21 12:32:22 +0200

    Function to show a matrix as image

--- cvtools.py
+++ cvtools.py
@@ -1,12 +1,20 @@
 import cv2
 import matplotlib.pyplot as plt
 
-def ipynb_show_image(image):
+def ipynb_show_image(image, title=''):
+  fig, ax = plt.subplots(figsize=(7, 7))
+  ax.set_title(title)
   plt.imshow(image)
   plt.axis('off')
 
 
-def ipynb_show_color_histogram(histogram, plot_title):
+def ipynb_show_matrix(matrix, title=''):
+  matrix_image = 255.0 * (matrix - np.min(matrix)) / (
+      np.max(matrix) - np.min(matrix))
+  ipynb_show_image(matrix_image, title)
+
+
+def ipynb_show_color_histogram(histogram, plot_title=''):
   fig, ax = plt.subplots(figsize=(12, 1.5))
   ax.set_title(plot_title)
   ax.bar(range(len(histogram)), histogram, color='r', width=1)
