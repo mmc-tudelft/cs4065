87105ea1b2ade9806f70d813a3a18ca43e5c3512
Alessio Bazzica <alessio.bazzica@gmail.com>
2016-04-21 11:34:07 +0200

    Function to show color histograms

--- cvtools.py
+++ cvtools.py
@@ -4,3 +4,9 @@ import matplotlib.pyplot as plt
 def ipynb_show_image(image):
   plt.imshow(image)
   plt.axis('off')
+
+
+def show_color_histogram(histogram, plot_title):
+  fig, ax = plt.subplots(figsize=(12, 1.5))
+  ax.set_title(plot_title)
+  ax.bar(range(len(histogram)), histogram, color='r', width=1)
