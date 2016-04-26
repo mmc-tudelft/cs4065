028b69a5be3dd4480579760f5ab05f4d0fbbc183
Alessio Bazzica <alessio.bazzica@gmail.com>
2016-04-26 12:55:11 +0200

    Custom config.py file

--- datasets.py
+++ datasets.py
@@ -1,15 +1,15 @@
 import os
 
-class CS4065_Dataset(object):
-  PATH_DATASETS = '/var/opt/pynb_data/cs4065'
+from config import PATH_DATA
 
+class CS4065_Dataset(object):
   def __init__(self):
     pass
 
   @classmethod
   def get_poster_images(cls):
     images = []
-    path_to_images = os.path.join(cls.PATH_DATASETS, 'poster_images')
+    path_to_images = os.path.join(PATH_DATA, 'poster_images')
     for image_filename in os.listdir(path_to_images):
       image_filepath = os.path.join(path_to_images, image_filename)
       if not os.path.isfile(image_filepath):
